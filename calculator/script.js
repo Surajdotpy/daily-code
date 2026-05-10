1f9dc2bc445341ce62f2e6a93cc53cd03a2dae76
Suraj Kumar <itssurajkumar3@gmail.com>
2026-05-10 21:20:13 +0530

    refactor code

--- calculator/script.js
+++ calculator/script.js
@@ -20,17 +20,24 @@ buttons.forEach(button => {
        };
        if(value === "DEL"){
         display.innerText = display.innerText.slice(0, -1);
+        if (display.innerText === "") display.innerText = "0";
+
+        if (!isSecond) {
+        firstNumber = firstNumber.slice(0, -1);
+      } else {
+        secondNumber = secondNumber.slice(0, -1);
+      }
         return;
        };
        if(["+","-","*","/","%"].includes(value)){
             firstNumber = display.innerText 
             operator = value;
             isSecond = true;
-            display.innerText = "";
+            display.innerText += value;
             return;
        }
        if (value === "="){
-        secondNumber = display.innerText;
+     
         if(firstNumber === "" || secondNumber === "" || operator === ""){
             return;
         }
@@ -45,19 +52,31 @@ buttons.forEach(button => {
         } else if(operator === "*"){
             result = num1 * num2;
         } else if(operator === "/"){
-            result = num1 / num2;
+            result = num2 !== 0 ? num1 / num2 : "Error";
         } else if(operator === "%"){
             result = num1 % num2;
         }
 
         display.innerText = result;
-       
-        firstNumber = ""
+        firstNumber = String(result)
         secondNumber = ""
         operator = ""
         isSecond = false;
+        return;
         
        }
+        if (display.innerText === "0" && !isSecond) {
+            display.innerText = "";
+        }
+
+        if (!isSecond) {
+            firstNumber += value;
+        } else {
+            secondNumber += value;
+        }
+    
+
+
        display.innerText += value;
 
        });
